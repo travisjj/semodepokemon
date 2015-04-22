645bac2732ec8bcba1261f2fa766979a0e62621a
travisjj <im@emailtravis.com>
2015-04-22 12:44:58 -0700

    Update mod.js

--- mod.js
+++ mod.js
@@ -1,14 +1,160 @@
+var modObj = {
+    "Academia": ["aeismail", "eykanal", "ff524", "StrongBad"],
+    "Amateur Radio": ["Amber", "Michael Kjörling", "PearsonArtPhoto"],
+    "Android Enthusiasts": ["Bryan Denny", "eldarerathis", "onik", "Matthew Read"],
+    "Anime & Manga": ["ʞɹɐzǝɹ", "Madara Uchiha", "senshin"],
+    "Arduino": ["Annonomus Penguin", "Peter R. Bloomfield", "sachleen"],
+    "Arqade": ["badp", "Juan Manuel Formoso", "Raven Dreamer", "Ashley Nunn", "fredley", "LessPop_MoreFizz", "StrixVaria"],
+    "Ask Different": ["bmike", "Daniel", "Ian C.", "patrix", "stuffe"],
+    "Ask Patents": ["Matthew Haugen"],
+    "Ask Ubuntu": ["Oli", "Stefano Palazzo", "Marco Ceppi", "fossfreedom", "jrg", "RolandiXor", "jokerdino", "Luis Alvarado", "Seth", "Mitch"],
+    "Astronomy": ["called2voyage", "Donald.McLean", "RhysW"],
+    "Aviation": ["egid", "Lnafziger", "voretaq7"],
+    "Beer": ["Andrew Cheong", "Fishtoaster", "LessPop_MoreFizz"],
+    "Biblical Hermeneutics": ["Caleb", "Dan", "Susan"],
+    "Bicycles": ["Gary.Ray", "freiheit", "zenbike", "jimirings"],
+    "Biology": ["kmm", "Mad Scientist", "Rory M"],
+    "Bitcoin": ["Murch", "Nick ODell", "StephenM347"],
+    "Blender": ["A Wild RolandiXor", "Dan the Man", "ideasman42", "iKlsR"],
+    "Board & Card Games": ["ire_and_curses", "mafu", "Pat Ludwig"],
+    "Buddhism": ["Andrei Volkov", "Hrafn", "yuttadhammo"],
+    "Chemistry": ["F'x", "jonsca", "ManishEarth"],
+    "Chess": ["Andrew", "Daniel", "ETD"],
+    "Chinese Language": ["Alenanno", "Don Kirkby", "Growler", "xiaohouzi79"],
+    "Christianity": ["wax eagle", "Mason Wheeler", "Caleb", "El'endia Starman", "David Stratton"],
+    "Code Review": ["200_success", "Jamal", "rolfl"],
+    "Coffee": ["fredley", "hoc_age", "Sam Whited"],
+    "Cognitive Sciences": ["Artem Kaznatcheev", "Jeromy Anglim", "Josh Gitlin", "Steven Jeuris"],
+    "Community Building": ["AJ Henderson", "Andy", "Annonomus Penguin"],
+    "Computational Science": ["Geoff Oxberry", "Paul"],
+    "Computer Science": ["Gilles", "Patrick87", "Raphael"],
+    "Craft CMS": ["Anna_MediaGirl", "Lindsey D", "Matt Stein"],
+    "Cross Validated": ["mbq", "chl", "whuber", "Peter Flom", "cardinal", "Glen_b", "Scortchi"],
+    "Cryptography": ["Hendrik Brummermann", "mikeazo", "Paŭlo Ebermann"],
+    "Data Science": ["AsheeshR", "Rubens", "Sean Owen"],
+    "Database Administrators": ["mrdenny", "Jack Douglas", "jcolebrand", "JNK", "Aaron Bertrand"],
+    "Drupal Answers": ["googletorp", "kiamlaluno", "MPD", "Clive"],
+    "Earth Science": ["casey", "gerrit", "hichris123"],
+    "Ebooks": ["Ed Cottrell", "Jason Down", "Nathan Osman"],
+    "Economics": ["Jason Nichols", "jmbejara", "Ubiquitous"],
+    "Electrical Engineering": ["Kortuk", "W5VO", "clabacchio", "Dave Tweed", "Nick Alexeev"],
+    "Emacs": ["Dan", "Jonathan Leech-Pepin", "Malabarba"],
+    "Engineering": ["Air", "GlenH7", "Trevor Archibald"],
+    "English Language & Usage": ["RegDwigнt", "nohat", "waiwai933", "simchona", "Kit Z. Fox", "Andrew Leach", "Yoichi Oishi", "Matt E. Эллен"],
+    "English Language Learners": ["J.R.", "Matt", "waiwai933", "WendiKidd"],
+    "Expatriates": ["Mark Mayo", "StrongBad", "SztupY"],
+    "ExpressionEngine® Answers": ["Anna_MediaGirl", "Alex Kendrick", "Derek Hogue"],
+    "Freelancing": ["Amelia", "Canadian Luke", "jmort253"],
+    "French Language": ["Evpok", "Gilles", "Stéphane Gimenez"],
+    "Game Development": ["Tetrad", "Noctrine", "Josh Petrie", "Byte56"],
+    "Gardening & Landscaping": ["Niall C.", "Tea Drinker", "wax eagle"],
+    "Genealogy & Family History": ["CRSouser", "Jan Murphy", "PolyGeo"],
+    "Geographic Information Systems": ["whuber", "Mapperz", "underdark", "iant", "PolyGeo", "Aaron"],
+    "German Language": ["RegDwight", "splattne", "Takkat"],
+    "Graphic Design": ["Darth_Vader", "JohnB", "Yisela", "Randy Jones"],
+    "Hinduism": ["cheenbabes", "Keshav Srinivasan", "Mr. Alien", "senshin"],
+    "History": ["ihtkwot", "Steven Drennon", "T.E.D."],
+    "History of Science and Mathematics": ["Danu", "HDE 226868", "Logan M"],
+    "Home Improvement": ["ChrisF", "BMitch", "Tester101", "Niall C."],
+    "Homebrewing": ["Denny Conn", "jsled", "mdma"],
+    "Information Security": ["AviD", "Rory Alsop", "Hendrik Brummermann", "Jeff Ferland"],
+    "Islam": ["Ansari", "Bleeding Fingers", "goldPseudo"],
+    "Italian Language": ["Charo", "I.M.", "martina"],
+    "Japanese Language": ["Dave", "jkerian", "snailboat"],
+    "Joomla": ["David Fritsch", "Harald Leithner", "Valentin Despa"],
+    "LEGO® Answers": ["Ambo100", "Joubarc", "Zhaph - Ben Duguid"],
+    "Lifehacks": ["J. Musser", "michaelpri", "Mooseman"],
+    "Linguistics": ["Alenanno", "Otavio Macedo", "prash"],
+    "Magento": ["Alex", "benmarks", "Marius"],
+    "Martial Arts": ["David H. Clements", "Matt Chan", "slugster"],
+    "Mathematica": ["Mr.Wizard", "rm -rf", "Verbeia"],
+    "Mathematics": ["Mariano Suárez-Alvarez", "mixedmath", "robjohn", "Arthur Fischer", "Michael Greinecker", "Alexander Gruber", "Daniel Fischer", "Jyrki Lahtonen", "Pedro Tamaroff"],
+    "Mathematics Educators": ["quid", "Sue VanHattum"],
+    "MathOverflow": ["Mariano Suárez-Alvarez", "Todd Trimble", "Ben Webster", "David Zureick-Brown", "François G. Dorais", "S. Carnahan", "Scott Morrison"],
+    "Mi Yodeya": ["Monica Cellio", "Double AA", "msh210"],
+    "Motor Vehicle Maintenance & Repair": ["Bob Cross", "Mark Johnson", "Move more comments link to top"],
+    "Movies & TV": ["iandotkelly", "Napoleon Wilson", "Ankit Sharma"],
+    "Music: Practice & Theory": ["Dr Mayhem", "Matthew Read", "NReilingh"],
+    "Network Engineering": ["Craig Constantine", "YLearn"],
+    "Open Data": ["Jeanne Holm", "Joe", "Patrick Hoefler"],
+    "Parenting": ["anongoodnurse", "Erica", "Rory Alsop"],
+    "Personal Finance & Money": ["JoeTaxpayer", "JohnFx"],
+    "Personal Productivity": ["Adam Wuerl", "Jeanne Boyarsky", "Rory Alsop"],
+    "Pets": ["James Jenkins", "John Cavan", "Zaralynda"],
+    "Philosophy": ["Joseph Weissman", "mixedmath", "stoicfury", "virmaior"],
+    "Photography": ["Rowland Shaw", "jrista", "chills42", "John Cavan"],
+    "Physical Fitness": ["Greg", "Ivo Flipse", "Matt Chan", "Nathan Wheeler"],
+    "Physics": ["David Z", "dmckee", "Qmechanic", "Manishearth"],
+    "Poker": ["Gaz Winter", "Radu Murzea", "Toby Booth"],
+    "Politics": ["Sam I am", "Yannis Rizos"],
+    "Programmers": ["ChrisF", "Yannis Rizos", "Thomas Owens", "maple_shaft", "World Engineer"],
+    "Programming Puzzles & Code Golf": ["dmckee", "Doorknob", "gnibbler", "Martin Büttner"],
+    "Project Management": ["jmort253", "Mark Phillips", "Tiago Cardoso"],
+    "Puzzling": ["Doorknob", "Emrakul", "Kevin"],
+    "Quantitative Finance": ["Bob Jansen", "olaker", "SRKX"],
+    "Raspberry Pi": ["goldilocks", "Jivings", "Steve Robillard"],
+    "Reverse Engineering": ["Igor Skochinsky", "Peter Andersson"],
+    "Robotics": ["Andrew", "Mark Booth", "ThomasH"],
+    "Role-playing Games": ["mxyzplk", "Brian Ballsun-Stanton", "SevenSidedDie", "wax eagle"],
+    "Russian Language": ["Alenanno", "Quassnoi", "shabunc"],
+    "Salesforce": ["Samuel De Rycke", "metadaddy", "LaceySnr"],
+    "Science Fiction & Fantasy": ["Keen", "Pureferret", "Kevin", "Richard", "Thaddeus"],
+    "Seasoned Advice": ["Aaronut", "rumtscho", "Jefromi"],
+    "Server Fault": ["Mark Henderson", "sysadmin1138", "voretaq7", "Michael Hampton", "Tom O'Connor", "Sven"],
+    "SharePoint": ["SPDoctor", "Robert Lindgren", "PirateEric", "BennySkogberg"],
+    "Signal Processing": ["datageist", "Peter K.", "Phonon"],
+    "Skeptics": ["Mad Scientist", "Konrad Rudolph", "Larian LeQuella", "Oddthinking", "Jamiec"],
+    "Software Quality Assurance & Testing": ["Bruce McLeod", "corsiKa", "testerab"],
+    "Software Recommendations": ["Gilles", "Journeyman Geek", "Undo"],
+    "Sound Design": ["AJ Henderson", "Arnoud Traa", "Jay Jennings", "JoshP", "Rory Alsop", "Warrior Bob"],
+    "Space Exploration": ["called2voyage", "PearsonArtPhoto", "TildalWave"],
+    "Spanish Language": ["Flimzy", "JoulSauron", "Joze"],
+    "Sports": ["edmastermind29", "jamauss", "Michael Myers"],
+    "Stack Apps": ["Nathan Osman", "Tim Stone"],
+      "Stack Exchange": ["Jeff Atwood", "Joel Spolsky", "Jarrod Dixon", "Geoff Dalgas", "David Fullerton", "Korneel Bouman", "Kevin Montrose", "Robert Cartaino", "Marc Gravell", "Ben Dumke-von der Ehe", "Matt Sherman", "Danny Miller", "Jason Punyon", "Nick Cardillo", "Kyle Brandt", "Jin Yang", "Jeff Szczepanski", "George Beech", "Nick Craver", "Nick Larsen", "Josh Heyer", "Greg Drinkwater", "Alex Miller", "Guy Zerega", "Abby Miller", "Sam Brand", "Laura Dobrzynski-Gessner", "Chris Jaeger", "Dimitar Stanimiroff", "Rachel Boyman", "Peter Grace", "Charles Bernoskie", "Anna Lear", "Steve Feldman", "Bethany Marzewski", "Dammand Cherry", "Ben Kiziltug", "Jay Greenbaum", "Matt Jibson", "Stefan Schwarzgruber", "Will Cole", "Sean Bave", "Robyn Wertman", "Bart Silverstrim", "Jay Hanlon", "Maxwell Applebaum", "Erin Gray", "Jordan Conner", "Adam DeLanoy", "Casey Ashenhurst", "Robert Brooks", "Maura Bradley", "Chris Martin", "Joe Humphries", "Max Horstmann", "Oded Coster", "Bryan Ross", "Valentina Perez", "Tim Post", "Derek Still", "Pieter DePree", "Paul Frey", "Todd Jenkins", "Marco Cecconi", "Jim Egan", "Pawel Michalak", "Tom Limoncelli", "Michael Dillon", "David Lislet", "Sara Rayman", "Monika Pradhan", "Philip Sireci", "Shanna Sobel", "Samo Prelog", "Jon Ericson", "Joel Bradley", "Tania Rahman", "Dean Grant", "Angela Nyman", "Joshua Hynes", "Kasra Rahjerdi", "Gabe Koscky", "Bret Copeland", "Arie Litovsky", "Kevin Chang", "Megan Spaans", "Whitney Dwyer", "Philip Camillo", "Jindrich Slisik", "Aurelien Gasser", "Alyssa Tomback", "Alex Cresswell", "Andrew Couch", "Brian Nickel", "Colleen McGarity", "Yaakov Ellis", "Ana Hevesi", "Noureddine Latrech", "Matthew Hertzfeldt", "Jill Ciaccia", "Tobias Schmidt", "Jon Chan", "Kristian Bright", "John LeClerc", "Rob Dandorph", "Stéphane Martin", "Jessica Genther", "Aaron Bata", "Jason Kowlowitz", "Shahed Kader", "Courtny Cotten", "Stephanie Huang", "Sean Durkin", "Lance Holloway", "Roberta Arcoverde", "Alex Warren", "Natasha Marsh", "Arnaud Kirkwood", "Jaime Kronick", "Alexa Scordato", "Jack Sinclair", "Sam Rouayrenc", "Rebecca Stein", "Jarriet Tambo", "Peter Tarr", "Shane Madden", "Sam Torno", "Mike McGranahan", "Jasmine Pengelly", "James Armbruster", "Dan O'Boyle", "Brenda Voloshin", "Griffin Sandberg", "Sam Nayrouz", "Jean Anstett", "Joshua Wrede", "Geoff McAdam", "Mark Villarreal", "Lowell Gruman", "Benjamin Weber", "Liam Ross", "Natalie How", "Meredith Ambinder", "David Haney", "Rory Gallard", "Beau Bergman", "Christian Nedjalkow", "Rita Cohen", "Bennet White", "Stuart Gowan", "Eli King", "Emily Kraus", "Cassie Stone", "Josh Maciel", "Katheryn Hurley", "Benny Xie", "Rachel Taylor", "Steven McMullen", "Jess Pardue", "Dean Ward", "Michael Dwyer", "Benjamin Rustom", "Michael Egan", "Matthew Dardenne", "Chloe Celand", "Rhakel Burwell", "Raquel Soares Ribeiro", "Greg Bray", "Andrew Dunne", "Natalia Radcliffe-Brine", "Angela Dominguez Del Val", "Asa Hope", "Joseph Lippens", "Kelcey Johnson", "Nada Elkady", "Nicholas Chabanovskii", "John Muskett", "Jake Shirek", "Kevin Krom", "Danielle Davisson", "Alex Bass", "Lillian Cortez", "Steve Trout", "Adam Tzagournis", "Kelli Ward", "Chris Ingram", "Heidi Hays", "Noah Neuman", "Mackenzie Ralston", "Lauren Roemer", "Joe Wilkie", "Kurtis Beavers", "Johnny Anderson", "Mary Croskery", "Anton Boner", "Max Fenn", "Alex Matteson", "Tommy Larsen", "Sarah Bregman", "Jennie Lee", "Donna Choi"],
+    "Stack Overflow": ["Bill the Lizard", "Michael Myers", "Robert Harvey", "BoltClock", "casperOne", "George Stocker", "Brad Larson", "ThiefMaster", "ChrisF", "Gordon", "Flexo", "minitech", "Bohemian", "0x7fffffff", "bluefeet", "meagar", "Martijn Pieters", "Jeremy Banks"],
+    "Stack Overflow em Português": ["bfavaretto", "utluiz"],
+    "Stack Overflow на русском": ["Barmaley", "Nofate"],
+    "Startups": ["connor", "Matthew Haugen", "Nick Wilde"],
+    "Super User": ["Ivo Flipse", "random", "Sathya", "studiohack", "nhinkle", "slhck", "Oliver Salzburg", "Daniel Beck", "Journeyman Geek", "Mokubai"],
+    "Sustainable Living": ["Earthliŋ", "EnergyNumbers", "THelper"],
+    "TeX - LaTeX": ["Joseph Wright", "Stefan Kottwitz", "Martin Scharrer"],
+    "The Great Outdoors": ["Kevin", "Rory Alsop", "Russell Steen", "studiohack"],
+    "The Workplace": ["jmort253", "Monica Cellio", "enderland"],
+    "Theoretical Computer Science": ["Suresh Venkat", "Lev Reyzin", "Artem Kaznatcheev"],
+    "Tor": ["Jens Kubieziel", "Sam Whited", "Stig Atle Steffensen"],
+    "Travel": ["Ankur Banerjee", "mindcorrosive", "RoflcoptrException"],
+    "Tridion": ["Alvin Reyes", "Bart Koopman", "Rob Stevenson-Leggett"],
+    "Unix & Linux": ["Michael Mrozek", "xenoterracide", "terdon", "slm"],
+    "User Experience": ["Rahul", "Ben Brocka", "JonW", "JohnGB", "BennySkogberg"],
+    "Video Production": ["AJ Henderson", "Dr Mayhem", "Professor Sparkles"],
+    "Web Applications": ["phwd", "ChrisF", "Barry", "Sathya", "jonsca"],
+    "Webmasters": ["John Conde", "Stephen Ostermiller", "bybe", "dan"],
+    "Windows Phone": ["row1", "Rowland Shaw", "Vitor Canova"],
+    "Woodworking": ["bowlturner", "drs", "rob"],
+    "WordPress Development": ["Rarst", "EAMann", "toscho", "Bainternet", "kaiser", "s_ha_dum", "Tom J Nowell"],
+    "Worldbuilding": ["Michael Kjörling", "Monica Cellio", "Tim B"],
+    "Writers": ["Monica Cellio", "Neil Fein", "Standback"],
+    "スタック・オーバーフロー": ["ento", "tomute", "unarist"]
+};
+
+for(var k in modObj){
+ var opt = $("<option>").val(k).text(k);
+ $("#regions").append(opt);
+}
+
 var trainer;
 var pokedex = ["Pickachu"];
+var mods;
 $("#b").click(function(){
    trainer = $("#name").val();
    $("#nameArea").text(trainer);
    $("#selector").fadeIn();
 });
 
-var mods = ["Bill the Lizard", "Michael Myers", "Robert Harvey", "BoltClock", "casperOne", "George Stocker", "Brad Larson", "ThiefMaster", "ChrisF", "Gordon", "Flexo", "minitech", "Bohemian", "0x7fffffff", "bluefeet", "meagar", "Martijn Pieters", "Jeremy Banks"];
 var s = $("#s");
 $("#i").click(function(){
+  mods = modObj[$("#regions").val()];
   var self = this;
   self.disabled = true;
   var r = (Math.random()*mods.length)|0;
@@ -27,8 +173,12 @@ $("#i").click(function(){
   setTimeout(function(){
       var delay = 5000;
       if( yourRoll < theirRoll ){
-       $("#result").text("You Win!! Adding "+mods[r]+" to your Pokédex");
+       var message = "You Win!! Adding "+mods[r]+" to your Pokédex";
        var modPoke = mods.splice(r,1);
+       if(mods.length == 0){
+        message+= "<br>You also defeated "+$("#regions").val()+"!!!! <i>epic</i>";       
+       }
+       $("#result").html(message);
        pokedex.push(modPoke);
        s.append("<option value="
                 +(pokedex.length-1)
@@ -43,7 +193,10 @@ $("#i").click(function(){
           $("#them").text("");
           $("#result").text("");
           self.disabled = false;
-          if(mods.length == 0 || true){
+          if(mods.length == 0){
+           $("#regions option:selected").remove();
+          }
+          if($("#regions option").length == 0){
           $("body").html('<div>OMG YOU WON! YOU CAUGHT ALL THE MODS!!</div><div><img src="http://i.imgur.com/atz81.jpg" />'); 
           }
       },delay);
